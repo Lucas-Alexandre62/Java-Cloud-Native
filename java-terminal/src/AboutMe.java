76f56a83e440979c69716dee5bcd11440a90d754
Lucas Alexandre <67240088+Lucas-Alexandre62@users.noreply.github.com>
2025-03-18 16:48:31 -0300

    Update AboutMe.java

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
 
         System.out.println("Ola, me chamo: " + nome + sobrenome);
         System.out.println("Tenho " + idade + " anos");
