25344c10e7964e2eabcf5a08628fcbf73c78ece6
Lucas Alexandre <67240088+Lucas-Alexandre62@users.noreply.github.com>
2025-03-18 11:28:34 -0300

    Update TiposVariaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean bool = true; // valores true ou false - 2 bits
+        short s = 10; // valores de -32.768 a 32.767 - 16 bits
+        int i = 10; // valores inteiros de -2.147.483.648 a 2.147.483.647 - 32 bits
+        long l = 10L; // valores inteiros de -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807 - 64 bits
+        double d = 10.0; // valores decimais de 4.9E-324 a 1.7976931348623157E308 - 64 bits
+        float f = 10.0f; // valores decimais de 1.4E-45 a 3.4028235E3s - 32 bits
+        char c = 'a'; // valores letra de 0 a 65.535 - 16 bits
+        byte b = 10; // valores de -128 a 127 - 8 bits
+        String str = "Hello World"; // sequência de caracteres
     }
 }
