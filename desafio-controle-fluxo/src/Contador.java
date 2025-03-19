91da16df903f851ea0300951e95623cbc57f0ccf
Lucas Alexandre <67240088+Lucas-Alexandre62@users.noreply.github.com>
2025-03-19 13:47:02 -0300

    update Contador.java

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -23,6 +23,8 @@ public class Contador {
 			throw new ParametrosInvalidosException();
 		}
 		int contagem = parametroDois - parametroUm;
+        
+        //realizar o for para imprimir os números com base na variável contagem
 		for (int i = 0; i <= contagem; i++) {
             System.out.println("Imprimindo o numero " + i);
         }
